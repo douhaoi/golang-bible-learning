d7bd91d5df9e4188542f8475854fe1109f194b00
douhao <douhaoi@126.com>
2025-12-15 01:28:31 +0800

    清理无用的文件依赖

--- src/routes/chapters/index.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import ChapterList from '../../pages/ChapterList'
-
-export const Route = createFileRoute()({
-  component: () => <ChapterList />,
-})
